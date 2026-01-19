ed449fc02dde66f957ff0cd4673016d4a17ab8f1
Hannu <104832348+happla@users.noreply.github.com>
2026-01-20 00:05:13 +0200

    Refactor LED control in blink.cpp
    Refactor LED control to use LED_PIN constant and adjust timing.

--- BLINK/blink.cpp
+++ BLINK/blink.cpp
@@ -1,26 +1,22 @@
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_log.h"
 
-#define LED_BUILTIN GPIO_NUM_8  // onboard blue LED
-static const char* TAG = "BLINK";
+#define LED_PIN GPIO_NUM_8  // onboard blue LED
 
 extern "C" void app_main(void)
 {
-    gpio_reset_pin(LED_BUILTIN);
-    gpio_set_direction(LED_BUILTIN, GPIO_MODE_OUTPUT);
-
-    ESP_LOGI(TAG, "Starting blink...");
+    gpio_reset_pin(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
 
     while (true)
     {
-        gpio_set_level(LED_BUILTIN, 0);  // LED ON (active-low)
-        ESP_LOGI(TAG, "LED ON");
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        //LED ON
+        gpio_set_level(LED_PIN, 0); 
+        vTaskDelay(pdMS_TO_TICKS(500)); //0.5s
 
-        gpio_set_level(LED_BUILTIN, 1);  // LED OFF
-        ESP_LOGI(TAG, "LED OFF");
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        //LED OFF
+        gpio_set_level(LED_PIN, 1);
+        vTaskDelay(pdMS_TO_TICKS(500)); //0.5s
     }
 }
